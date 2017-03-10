45344c612cf23387a5470beaf63d21911d2fc4f8
milena <mile.card195@gmail.com>
2017-03-10 18:11:45 -0500

    Desarrollo aprendiendo Js

---
+++ JAVASCRIPT/Ejercicio1/js/script.js
@@ -0,0 +1,8 @@
+alert("hola mundo!")
+var x = 2+3;
+alert(x);
+
+function meLlamaron(){    //alert(document.getElementById('nombre').value);
+//document.getElementById("id123").innerHTML="wtf"
+document.getElementById("id123").innerHTML="<p>Hola<p>"
+}
